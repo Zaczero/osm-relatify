1821f1e23750d814a080dc89cd14de96a6eeb976
Kamil Monicz <kamil@monicz.pl>
2023-07-04 19:36:04 +0200

    Docker

--- web/shell.nix
+++ web/shell.nix
@@ -3,6 +3,7 @@
 pkgs.mkShell {
   buildInputs = with pkgs; [
     gnumake
+    gnused
     python311
     pipenv
     esbuild
@@ -12,7 +13,8 @@ pkgs.mkShell {
     export LD_LIBRARY_PATH="${stdenv.cc.cc.lib}/lib:$LD_LIBRARY_PATH"
     export PIPENV_VENV_IN_PROJECT=1
     export PIPENV_VERBOSITY=-1
-    [ ! -f ".venv/bin/activate" ] && pipenv sync --dev
-    exec pipenv shell --fancy
+    [ -v DOCKER ] && [ ! -f .venv/bin/activate ] && pipenv sync
+    [ ! -v DOCKER ] && [ ! -f .venv/bin/activate ] && pipenv sync --dev
+    [ ! -v DOCKER ] && exec pipenv shell --fancy
   '';
 }
