3cbfd37d54e457cfec37834d57f70cb392de1ce2
Kamil Monicz <kamil@monicz.pl>
2023-08-19 10:56:24 +0200

    nix-build

---
+++ web/default.nix
@@ -0,0 +1,53 @@
+{ pkgs ? import <nixpkgs> { } }:
+
+with pkgs; let
+  shell = import ./shell.nix {
+    inherit pkgs;
+    isDocker = true;
+  };
+
+  python-venv = buildEnv {
+    name = "python-venv";
+    paths = [
+      (runCommand "python-venv" { } ''
+        mkdir -p $out/lib
+        cp -r "${./.venv/lib/python3.11/site-packages}"/* $out/lib
+      '')
+    ];
+  };
+in
+dockerTools.buildLayeredImage {
+  name = "docker.monicz.pl/osm-relatify";
+  tag = "latest";
+  maxLayers = 10;
+
+  contents = shell.buildInputs ++ [ python-venv ];
+
+  extraCommands = ''
+    set -e
+    mkdir app && cd app
+    cp "${./.}"/LICENSE .
+    cp "${./.}"/Makefile .
+    cp "${./.}"/*.py .
+    cp -r "${./.}"/models .
+    cp -r "${./.}"/static .
+    cp -r "${./.}"/templates .
+    export PATH="${esbuild}/bin:$PATH"
+    ${shell.shellHook}
+  '';
+
+  config = {
+    WorkingDir = "/app";
+    Env = [
+      "LD_LIBRARY_PATH=${lib.makeLibraryPath shell.buildInputs}"
+      "PYTHONPATH=${python-venv}/lib"
+      "PYTHONUNBUFFERED=1"
+      "PYTHONDONTWRITEBYTECODE=1"
+    ];
+    ExposedPorts = {
+      "8000/tcp" = { };
+    };
+    Entrypoint = [ "python" "-m" "uvicorn" "main:app" ];
+    Cmd = [ "--host" "0.0.0.0" ];
+  };
+}
