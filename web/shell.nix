ed037536e4b64cf84d398af7afa39d8b23019035
Kamil Monicz <kamil@monicz.dev>
2024-07-06 16:49:25 +0200

    Update packages

--- web/shell.nix
+++ web/shell.nix
@@ -2,7 +2,7 @@
 
 let
   # Update packages with `nixpkgs-update` command
-  pkgs = import (fetchTarball "https://github.com/NixOS/nixpkgs/archive/9693852a2070b398ee123a329e68f0dab5526681.tar.gz") { };
+  pkgs = import (fetchTarball "https://github.com/NixOS/nixpkgs/archive/4284c2b73c8bce4b46a6adf23e16d9e2ec8da4bb.tar.gz") { };
 
   pythonLibs = with pkgs; [
     stdenv.cc.cc.lib
